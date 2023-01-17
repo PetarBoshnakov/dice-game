5044f91ba85aeaf186d148679b76c4a359867c1b
Petar Boshnakov <pboshnakov@pmone.com>
2023-01-17 16:20:55 +0200

    updates to the man page

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ import classes
 
 def main():
     
-    help_info = "Nice to see you here. This is my lovely implementation of Liar's Dice game.\n\n\
-            -help, -help, h, -h: to get the help menu \n\
-            -start: to start the game \n\
-            -debug: to enter debug mode revealing system information on the player and the bot"
+    help_info = '''
+It's nice to see you here! This is my lovely implementation of Liar's Dice game.
+More information can be found on the repository page in GitHub here: 
+https://github.com/PetarBoshnakov/dice-game
+
+    -help, -help, h, -h: to get the help menu 
+    -start: to start the game 
+    -debug: to enter debug mode revealing system information on the player and the bot'''
 
     try:
         main_arg = sys.argv[1]
